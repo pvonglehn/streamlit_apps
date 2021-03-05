52f0ac03636d680000748367de0b290a81e943f4
Patrick von Glehn <p.vonglehn@carepay.com>
2021-03-05 11:14:44 +0100

    plot in altair function

--- basic_algorithms/app.py
+++ basic_algorithms/app.py
@@ -1,7 +1,8 @@
 from random import randrange
 from time import sleep
 
-import matplotlib.pyplot as plt
+import altair as alt
+import pandas as pd
 import streamlit as st
 
 
@@ -9,15 +10,6 @@ def initialize_array(n_elements, lower_limit, upper_limit):
     """initialize random array"""
 
     return [randrange(lower_limit, upper_limit) for _ in range(n_elements)]
-
-
-def plot(array):
-    """plot a barchart of array"""
-
-    fig, ax = plt.subplots(figsize=(FIGSIZE))
-    ax.bar(range(len(array)), array)
-
-    return fig
 
 
 def bubble_sort(array):
@@ -36,6 +28,24 @@ def bubble_sort(array):
         max_unsorted -= 1
 
 
+def plot_bar(source, title):
+    """plot barchart of array"""
+
+    source = pd.DataFrame(
+        {"array_index": range(len(array_snapshot)), "values": array_snapshot}
+    )
+    source["array_index"] = source["array_index"].astype(str)
+    sort_by = source["array_index"].values
+    chart = (
+        alt.Chart(source)
+        .mark_bar()
+        .encode(x=alt.X("array_index", sort=sort_by), y="values")
+        .properties(title=title)
+    )
+
+    return chart
+
+
 N_ELEMENTS = 20
 MIN = 0
 MAX = 100
@@ -46,8 +56,9 @@ array = initialize_array(N_ELEMENTS, MIN, MAX)
 
 array_gen = bubble_sort(array)
 
-chart = st.empty()
+chart_row = st.empty()
 
 for array_snapshot in array_gen:
-    chart.bar_chart(array_snapshot)
+    chart = plot_bar(array_snapshot, "Bubble Sort")
+    chart_row.altair_chart(chart)
     sleep(DELAY)
